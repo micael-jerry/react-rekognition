f3002881890ed4e189f034dcbaf74cddd3192553
Micael Jerry <hei.jerry.2@gmail.com>
2022-07-27 17:59:08 +0300

    update rekognition function

--- src/Components/Page.tsx
+++ src/Components/Page.tsx
@@ -1,30 +1,84 @@
 import React, {useState} from "react";
+import AWS, {Credentials} from 'aws-sdk' ;
 import RenderImage from "./RenderImage";
 
 const Page: React.FC<{}> = () => {
-    const [image,setImage] = useState<string | null | undefined | ArrayBuffer>(null);
+    const [image, setImage] = useState<string | null | undefined | ArrayBuffer>(null);
 
     const getImage = (e: React.ChangeEvent<HTMLInputElement>) => {
-        if(e.target.files == null)
+       if (e.target.files == null)
             return null;
         try {
+            let file = e.target.files[0];
             const reader = new FileReader();
-            reader.readAsDataURL(e.target.files[0]);
+            reader.readAsDataURL(file);
             reader.onload = () => {
                 setImage(reader.result);
-                if(e.target.files == null)
+                if (e.target.files == null)
                     return
             }
         } catch (err) {
             console.log(err);
         }
     }
+
+    function ProcessImage(event: React.ChangeEvent<HTMLInputElement>) {
+        AnonLog();
+        let file = event.target.files![0]
+        let reader = new FileReader();
+        reader.onload = (function (theFile) {
+            return function (e:any) {
+                AWS.config.region = 'eu-west-2';
+                let rekognition = new AWS.Rekognition();
+                let params = {
+                    Image: {
+                        Bytes: e.target.result
+                    },
+                    Attributes: [
+                        'ALL',
+                    ]
+                };
+                rekognition.detectFaces(params, function (err, data) {
+                    if (err) console.log(err, err.stack);
+                    else {
+                        console.log(data);
+                    }
+                });
+            }
+        })(file);
+        reader.readAsArrayBuffer(file);
+    }
+
+    function AnonLog() {
+        AWS.config.region = 'eu-west-2';
+        AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+            IdentityPoolId: 'eu-west-2:371cdf1c-657e-4e3f-a6a0-3cdcf905bfdc',
+        });
+        (AWS.config.credentials as Credentials).get(function () {
+            let accessKeyId = AWS.config.credentials?.accessKeyId;
+            let secretAccessKey = AWS.config.credentials?.secretAccessKey;
+            let sessionToken = AWS.config.credentials?.sessionToken;
+        });
+    }
+
     return (
         <div>
-            <div>
-                <input type={"file"} accept={"image/*,.png,.jpg,.jpeg"} onChange={getImage}/>
+            <div className={"container"}>
+                <div className="bg-light p-5 rounded">
+                    <h1>AWS REKOGNITION</h1>
+                    <p className="lead">
+                        Analyze faces
+                    </p>
+                    <input type={"file"} accept={"image/*,.png,.jpg,.jpeg"}
+                           className="btn btn-primary"
+                           onChange={ (e: React.ChangeEvent<HTMLInputElement>) => {
+                               getImage(e);
+                               ProcessImage(e);
+                           }}
+                    />
+                </div>
             </div>
-            <RenderImage image={image} />
+            <RenderImage image={image}/>
         </div>
     )
 }
--- src/Components/RenderImage.tsx
+++ src/Components/RenderImage.tsx
@@ -9,7 +9,39 @@ const RenderImage: React.FC<DisplayImageProps> = (props: DisplayImageProps) => {
     return (
         <>
             {typeof image === 'string'
-                ? (<img src={image} alt="image"/>)
+                ? (
+                    <>
+                        <hr className="featurette-divider"/>
+                        <div className="row featurette">
+                            <div className="col-md-7 order-md-2">
+                                <h2 className="featurette-heading">Face<span className="text-muted">description</span></h2>
+                                <div className="d-flex flex-column align-items-stretch flex-shrink-0 bg-white">
+                                    <div className="list-group list-group-flush border-bottom scrollarea">
+                                        <div className="list-group-item list-group-item-action py-3 lh-tight">
+                                            <div className="d-flex w-100 align-items-center justify-content-between">
+                                                <strong className="mb-1">List group item heading</strong>
+                                                <small className="text-muted">Tues</small>
+                                            </div>
+                                            <div className="col-10 mb-1 small">Some placeholder content in a paragraph
+                                                below the heading and date.
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-5 order-md-1">
+                                <img
+                                    className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
+                                    width={"500"}
+                                    height={"500"}
+                                    src={image}
+                                    alt={"image"}
+                                />
+                            </div>
+                        </div>
+                        <hr className="featurette-divider"/>
+                    </>
+                )
                 : (<></>)
             }
         </>
