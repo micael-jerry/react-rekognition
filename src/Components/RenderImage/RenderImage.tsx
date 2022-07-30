1a9299a2e7631bb07d74ef46af96276dfc79d9af
Micael Jerry <hei.jerry.2@gmail.com>
2022-07-30 21:13:08 +0300

    add face card

--- src/Components/RenderImage/RenderImage.tsx
+++ src/Components/RenderImage/RenderImage.tsx
@@ -5,7 +5,27 @@ import './RenderImage.css'
 
 const RenderImage: React.FC<DisplayImageProps> = (props: DisplayImageProps) => {
     const {image, result} = props;
-    
+
+    const [style,setStyle] = useState<any>(null);
+    const [boudingBox,setBoudingBox] = useState<any>(null);
+
+    useEffect(() => {
+        if(typeof result != "undefined" && result != null){
+            setBoudingBox(result![0][1]);
+        }
+    },[result])
+
+    useEffect(() => {
+        if(boudingBox != null) {
+            let image_render = document.getElementById('image-render')!.offsetWidth ;
+            setStyle({
+                "margin-top": boudingBox.Top/2 * image_render + "px",
+                "margin-left": boudingBox.Left/2 * image_render + "px",
+                "width": boudingBox.Width * image_render + "px",
+                "height": boudingBox.Height * image_render + "px"
+            })
+        }
+    },[boudingBox])
 
     return (
         <>
@@ -15,11 +35,19 @@ const RenderImage: React.FC<DisplayImageProps> = (props: DisplayImageProps) => {
                         <hr className="featurette-divider"/>
                         <div className="row">
                             <div className="col-md-5 content-center">
-                                <img id={"image-render"}
-                                    className="image bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
-                                    src={image}
-                                    alt={"face"}
-                                />
+                                <div className={"container-image"}>
+                                    <img id={"image-render"}
+                                         className="image bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
+                                         src={image}
+                                         alt={"face"}
+                                    />
+                                </div>
+                                {
+                                    (style !== null ?
+                                            ( <div className={"card-face"} style={style}></div>)
+                                            : (<></>)
+                                    )
+                                }
                             </div>
                             <div className="col-md-7">
                                 <h2 className="featurette-heading">
