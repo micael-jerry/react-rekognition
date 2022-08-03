37ea5db3652f9101e83a160e89ab36d1944172df
Micael Jerry <hei.jerry.2@gmail.com>
2022-08-03 09:15:46 +0300

    update result render

--- src/Components/Result/Result.tsx
+++ src/Components/Result/Result.tsx
@@ -5,50 +5,73 @@ import './Result.css';
 const Result: React.FC<{ result: resultType }> = (props) => {
     const {result} = props;
     return (
-        <div className="scroll-div list-group list-group-flush border-bottom">
-            <div className="list-group-item list-group-item-action py-3 lh-tight">
-                {
-                    (result?.slice(0, result!.length - 2) || []).map((item) => {
-                        let itemInItems = Object.entries(item[1]);
-                        return (
-                            <div key={item[0]}>
+        <div className={"list-group scroll-div"}>
+            {
+                (result?.slice(0, result!.length - 2) || []).map((item) => {
+                    let itemInItems = Object.entries(item[1]);
+                    return (
+                        <div className={"list-group list-group-flush border-bottom"}>
+                            <div className={"list-group-item list-group-item-action py-3 lh-tight"} key={item[0]}>
                                 <div className="d-flex w-100 align-items-center justify-content-between">
                                     <h5 className={"mb-2"}>{item[0]}</h5>
                                 </div>
-                                {
-                                    (itemInItems || []).map((itemInItem: [string, any]) => {
-                                        if (typeof itemInItem[1] !== "object") {
-                                            return (
-                                                <p key={itemInItem[0]}>
-                                                    {itemInItem[0]}: <small
-                                                    className="text-muted">{"" + itemInItem[1]}</small>
-                                                </p>
-                                            )
-                                        } else {
-                                            return (
-                                                <div key={itemInItem[0]} className="col-10 mb-1 small">
-                                                    {
-                                                        Object.entries(itemInItem[1]).map((itemInItemArray) => {
-                                                            console.log(itemInItemArray[0]+ " ====> "+itemInItemArray[1]);
-                                                            return (
-                                                                <p>{itemInItemArray[0]+ " ====> "+itemInItemArray[1]}</p>
+                                <ul className={"list-group listGroup"}>
+                                    {
+                                        (itemInItems || []).map((itemInItem: [string, any]) => {
+                                            if (typeof itemInItem[1] !== "object") {
+                                                return (
+                                                    <li key={itemInItem[0]}
+                                                        className={"list-group-item d-flex justify-content-between align-items-center"}>
+                                                        {itemInItem[0]}: <span
+                                                        className="text-muted">{"" + itemInItem[1]}</span>
+                                                    </li>
+                                                )
+                                            } else {
+                                                return (
+                                                    <>
+                                                        {
+                                                            (Object.keys(itemInItem[1]).length === 2) ? (
+                                                                <li key={itemInItem[1].Confidence}
+                                                                    className={"list-group-item d-flex justify-content-between align-items-center"}>
+                                                                    {itemInItem[1].Type}: <span
+                                                                    className={"text-muted"}>{itemInItem[1].Confidence}</span>
+                                                                </li>
+                                                            ) : (
+                                                                <>
+                                                                    <li key={itemInItem[1].X}
+                                                                        className={"list-group-item d-flex justify-content-between align-items-center"}>
+                                                                        {itemInItem[1].Type}:
+                                                                        <span>
+                                                                             X=<small
+                                                                            className={"text-muted"}>{itemInItem[1].X}</small>
+                                                                            <br/>
+                                                                            Y=<small
+                                                                            className={"text-muted"}>{itemInItem[1].Y}</small>
+                                                                        </span>
+                                                                    </li>
+                                                                </>
                                                             )
-                                                        })
-                                                    }
-                                                </div>
-                                            )
-                                        }
-                                    })
-                                }
+                                                        }
+                                                    </>
+                                                )
+                                            }
+                                        })
+                                    }
+                                </ul>
                                 <br/>
                             </div>
-                        )
-                    })
-                }
-                <div className="d-flex w-100 align-items-center justify-content-between">
-                    <strong
-                        className="mb-1">{result![result!.length - 1][0]}: <p>{"" + result![result!.length - 1][1]}</p>
-                    </strong>
+                        </div>
+                    )
+                })
+            }
+            <div className={"list-group list-group-flush border-bottom"}>
+                <div className={"list-group-item list-group-item-action py-3 lh-tight"}>
+                    <div className="d-flex w-100 align-items-center justify-content-between">
+                        <h5 className={"mb-1"}>
+                            {result![result!.length - 1][0]}:
+                        </h5>
+                        <h5 className={"mb-1"}>{"" + result![result!.length - 1][1]}</h5>
+                    </div>
                 </div>
             </div>
         </div>
